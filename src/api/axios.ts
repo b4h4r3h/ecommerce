9df4aebfaf82028d4bee080a410b0e281df23de4
bahare tourani <baharetourani1226@gmail.com>
2024-08-07 17:42:30 +0330

    add article link to axios file

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,14 +5,16 @@ export const axiosInstance = axios.create({
     baseURL:url
 })
 
+// https://medium.com/@barisberkemalkoc/axios-interceptor-intelligent-db46653b7303
+
 axiosInstance.interceptors.request.use(async (request) => {
     return request
 })
 
-// axiosInstance.interceptors.response.use(async (response) => {
-//     //TODO
-//     console.log(response)
-//     // if (response.data.isSuccess == false) {
-//     // }
-// })
+axiosInstance.interceptors.response.use(async (response) => {
+    //TODO
+    console.log("response",response)
+    // if (response.data.isSuccess == false) {
+    // }
+})
 
