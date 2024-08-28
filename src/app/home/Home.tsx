3410f41d7daa0ca10a8185856877e25cd33d80dc
b4h4r3h <b4h4r3ht@gmail.com>
2024-08-28 04:15:53 +0330

    add category

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -1,6 +1,5 @@
 import { useAllProducts } from "./hooks/useAllProducts"
 import "../../index.css"
-import SecondBanner from "./banner/SecondBanner";
 import ProductCard from "../components/ProductCard";
 import SkeletonImage from "antd/es/skeleton/Image";
 import BannerSwiper from "./bannerSwiper/BannerSwiper";
@@ -8,47 +7,46 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { Autoplay, Pagination, Navigation } from "swiper/modules";
-import FirstBanner from "./banner/FirstBanner";
+import { Pagination, Navigation } from "swiper/modules";
+import CategoriesCard from "./components/CategoriesCard";
 
 const Home: React.FC = () => {
     const { data: allProductsData, isError: allProductsError, isLoading: allProductsLoading } = useAllProducts();
 
-
     return (
         <>
             <BannerSwiper />
-
             <>
                 <h2 className="text-2xl font-bold text-center mt-6 mb-2"> Best Sellers!</h2>
                 {
-                allProductsLoading && (
-                    Array.from({ length: 10 }).map((item) => (
-                        <SkeletonImage />
-                    ))
-                )
-            }
-            <Swiper
-                slidesPerView={4}
-                spaceBetween={30}
-                // centeredSlides={true}
-                pagination={{
-                    clickable: true,
-                }}
-                navigation={true}
-                modules={[Navigation]}
-                className="product-swiper"
-            >
-                {allProductsData?.map((item) => (
-                    <div key={item?.id}>
-                        <SwiperSlide>
-                            <ProductCard image={item?.image} price={item?.price} title={item?.title} />
-                        </SwiperSlide>
+                    allProductsLoading && (
+                        Array.from({ length: 10 }).map((item) => (
+                            <SkeletonImage />
+                        ))
+                    )
+                }
+                <Swiper
+                    slidesPerView={4}
+                    spaceBetween={30}
+                    // centeredSlides={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Navigation]}
+                    className="product-swiper"
+                >
+                    {allProductsData?.map((item) => (
+                        <div key={item?.id}>
+                            <SwiperSlide>
+                                <ProductCard image={item?.image} price={item?.price} title={item?.title} />
+                            </SwiperSlide>
 
 
-                    </div>
-                ))}
-            </Swiper>
+                        </div>
+                    ))}
+                </Swiper>
+                <CategoriesCard/>
             </>
         </>
     )
--- src/reactQueryUtilities/queryKeys.ts
+++ src/reactQueryUtilities/queryKeys.ts
@@ -1,5 +1,6 @@
 export const queryKeys = Object.freeze({
     product: {
-        GET_ALL_PRODUCTS: "getAllProducts"
+        GET_ALL_PRODUCTS: "get-all-products",
+        GET_ALL_CATEGORIES: "get-all-categories"
     }
 })
