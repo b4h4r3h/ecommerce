d43402a4a0090e737cf484821eb201cc7e320a75
bahare tourani <baharetourani1226@gmail.com>
2024-09-08 16:09:36 +0330

    add hasBack to title

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -1,6 +1,14 @@
-const Title: React.FC<{ title: string }> = ({ title }) => (
-  <h3 className="text-2xl font-bold text-center mt-12 mb-4 text-text-dark">
-    {title}
-  </h3>
+const Title: React.FC<{ title: string; hasBack: boolean }> = ({
+  title,
+  hasBack,
+}) => (
+  <div>
+    {hasBack &&
+    
+    }
+    <h3 className="text-2xl font-bold text-center mt-12 mb-4 text-text-dark">
+      {title}
+    </h3>
+  </div>
 );
 export default Title;
