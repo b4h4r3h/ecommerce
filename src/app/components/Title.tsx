3f59328182d9f05622d1a7a0f5e170ea9cca1a1c
bahare tourani <baharetourani1226@gmail.com>
2024-09-17 11:47:25 +0330

    fix categories responsive

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -1,11 +1,11 @@
-const Title: React.FC<{ title: string; hasBack: boolean }> = ({
+const Title: React.FC<{ title: string; hasBack?: boolean }> = ({
   title,
   hasBack,
 }) => (
   <div>
-    {hasBack &&
+    {/* {hasBack &&
     
-    }
+    } */}
     <h3 className="text-2xl font-bold text-center mt-12 mb-4 text-text-dark">
       {title}
     </h3>
--- src/app/home/components/CategoriesCard.tsx
+++ src/app/home/components/CategoriesCard.tsx
@@ -41,7 +41,7 @@ const CategoriesCard: React.FC = () => {
   }
 
   return (
-    <section className="flex justify-center">
+    <section className="grid grid-cols-2 gap-y-6 md:flex md:justify-center">
       {allCategoriesData?.map((item, i) => {
         let content;
         switch (item) {
@@ -53,7 +53,7 @@ const CategoriesCard: React.FC = () => {
                     className="block icon-[solar--display-outline] text-green-800 w-9 h-9 sm:w-10 sm:h-10 md:w-12 md:h-12 lg:w-14 lg:h-14"
                   ></span>
                 </Link>
-                <p className="w-16 sm:w-28 lg:w-32 sm:text-sm lg:text-base text-center">electronics</p>
+                <p className="sm:w-28 lg:w-32 sm:text-sm lg:text-base text-center">electronics</p>
               </>
             );
             break;
@@ -65,7 +65,7 @@ const CategoriesCard: React.FC = () => {
                     className="block icon-[solar--crown-outline] text-rose-600 w-9 h-9 sm:w-10 sm:h-10 md:w-12 md:h-12 lg:w-14 lg:h-14"
                   ></span>
                 </Link>
-                <p className="w-16 sm:w-28 lg:w-32 sm:text-sm lg:text-base text-center">jewelery</p>
+                <p className="sm:w-28 lg:w-32 sm:text-sm lg:text-base text-center">jewelery</p>
               </>
             );
             break;
@@ -77,7 +77,7 @@ const CategoriesCard: React.FC = () => {
                     className="block icon-[solar--t-shirt-outline] text-sky-800 w-9 h-9 sm:w-10 sm:h-10 md:w-12 md:h-12 lg:w-14 lg:h-14"
                   ></span>
                 </Link>
-                <p className="w-16 sm:w-28 lg:w-32 sm:text-sm lg:text-base text-center">men's clothing</p>
+                <p className="w-28 sm:w-28 lg:w-32 sm:text-sm lg:text-base text-center">men's clothing</p>
               </>
             );
             break;
@@ -89,7 +89,7 @@ const CategoriesCard: React.FC = () => {
                     className="block icon-[solar--skirt-outline] text-fuchsia-800 w-9 h-9 sm:w-10 sm:h-10 md:w-12 md:h-12 lg:w-14 lg:h-14"
                   ></span>
                 </Link>
-                <p className="w-16 sm:w-28 lg:w-32 sm:text-sm lg:text-base text-center">women's clothing</p>
+                <p className="w-28 sm:w-28 lg:w-32 sm:text-sm lg:text-base text-center">women's clothing</p>
               </>
             );
             break;
